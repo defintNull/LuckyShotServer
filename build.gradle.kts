eb686b0a93e3d7d8e94b6761b69d7e8f7842f048
defintNull / Lorenzo + D'Angelo <lorenzo.dangelo.mail@gmail.com>
2025-01-08 11:59:37 +0100

    First Push

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    id("application")
+}
+
+application {
+    mainClass = "org.luckyshotserver.Main"
+}
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "org.luckyshotserver.Main"
+    }
+    configurations["runtimeClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+
+group = "org.luckyshotserver"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.hibernate:hibernate-core:6.6.3.Final")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.3.2")
+    implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.24.2")
+    implementation("org.springframework.security:spring-security-crypto:5.8.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
